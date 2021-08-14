1414b64a9b28a26865c17554537415eb01535eaa
Vadim Prokoshin <vpro85@gmail.com>
2021-08-14 03:50:18 +0200

    Modal Window for Create Post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
 
@@ -13,6 +15,7 @@ function App() {
     ])
 
     const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
         if (filter.sort) {
@@ -27,6 +30,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -35,8 +39,12 @@ function App() {
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <hr style={{margin: '15px 0'}}/>
+            <MyButton style={{marginTop: "30px"}} onClick={()=>setModal(true)}>Create modal</MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+
+            {/*<hr style={{margin: '15px 0'}}/>*/}
             <PostFilter filter={filter} setFilter={setFilter}/>
             <PostList posts={sortedAndSearchedPosts} title={"JS Posts list"} remove={removePost}/>
         </div>
