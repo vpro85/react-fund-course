0537ab8838c6a005d103d93ce84d4ce5451a9002
Vadim Prokoshin <vpro85@gmail.com>
2021-08-14 02:44:16 +0200

    Add sort and search for posts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description'},
-        {id: 2, title: 'Javascript 2', body: 'Description'},
-        {id: 3, title: 'Javascript 3', body: 'Description'},
+        {id: 1, title: 'Javascript', body: 'Front'},
+        {id: 2, title: 'Python', body: 'Back'},
+        {id: 3, title: 'Assembler', body: 'Base'},
     ])
+
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortedPosts = useMemo(() => {
+        // console.log("getSortedPost executed")
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return posts;
+    }, [selectedSort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()));
+    }, [searchQuery, sortedPosts])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -20,12 +36,24 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        // setPosts(sortedPosts)
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
             <div>
+                <MyInput
+                    placeholder={"Search..."}
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
                 <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
                     defaultValue="Sort by"
                     options={[
                         {value: "title", name: "by title"},
@@ -33,8 +61,8 @@ function App() {
                     ]}
                 />
             </div>
-            {posts.length !== 0 ?
-                <PostList posts={posts} title={"JS Posts list"} remove={removePost}/>
+            {sortedAndSearchedPosts.length ?
+                <PostList posts={sortedAndSearchedPosts} title={"JS Posts list"} remove={removePost}/>
                 :
                 <h1 style={{textAlign: "center"}}>Page is empty</h1>
             }
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 const MySelect = ({options, defaultValue, value, onChange}) => {
     return (
-        <select>
+        <select
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
             <option disabled value="">{defaultValue}</option>
             {options.map(option => {
-                return <option key={option.value} value={option.value}>{option.name}</option>
+                return <option key={option.value} value={option.value}>
+                    {option.name}
+                </option>
             })}
         </select>
     );
