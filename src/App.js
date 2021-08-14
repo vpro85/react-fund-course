7238824c55418846e1c821b35e23543886ea8be6
Vadim Prokoshin <vpro85@gmail.com>
2021-08-14 05:57:33 +0200

    useEffect, loading data from server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,6 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
 
@@ -14,9 +15,18 @@ function App() {
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    useEffect(()=> {
+        fetchPosts()
+    },[])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false)
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data);
     }
 
     const removePost = (post) => {
@@ -25,6 +35,7 @@ function App() {
 
     return (
         <div className="App">
+            <MyButton onClick={fetchPosts}>Get posts</MyButton>
             <MyButton style={{marginTop: "30px"}} onClick={() => setModal(true)}>Create modal</MyButton>
             <MyModal visible={modal} setVisible={setModal}>
                 <PostForm create={createPost}/>
