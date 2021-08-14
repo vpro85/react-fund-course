c54f8a1b0b410bb03e1e57ba7c747db32efab931
Vadim Prokoshin <vpro85@gmail.com>
2021-08-14 10:07:18 +0200

    Posts loading indicator, Loader component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
 
@@ -14,10 +15,11 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostsLoading, setIsPostsLoading] = useState(true);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetchPosts()
-    },[])
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -25,8 +27,10 @@ function App() {
     }
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data);
+        setIsPostsLoading(true);
+        const posts = await PostService.getAll();
+        setPosts(posts);
+        setIsPostsLoading(false);
     }
 
     const removePost = (post) => {
@@ -42,7 +46,11 @@ function App() {
             </MyModal>
 
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <PostList posts={sortedAndSearchedPosts} title={"JS Posts list"} remove={removePost}/>
+            {
+                isPostsLoading
+                    ? <div style={{display: "flex", justifyContent:"center", marginTop:50}}><Loader/></div>
+                    : <PostList posts={sortedAndSearchedPosts} title={"JS Posts list"} remove={removePost}/>
+            }
         </div>
     );
 }
