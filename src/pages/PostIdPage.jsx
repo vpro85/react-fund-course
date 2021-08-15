de1e358aa4d33e44bc6aaf557542d6fbaaab1a64
Vadim Prokoshin <vpro85@gmail.com>
2021-08-15 19:14:14 +0200

    Add Comments on PostById page

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,14 +7,20 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
 
     const [fetchPostById, isLoading, error] = useFetching(async (id) => {
         const response = await PostService.getById(id);
         setPost(response.data);
     });
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data);
+    });
 
     useEffect(() => {
         fetchPostById(params.id);
+        fetchComments(params.id);
     }, [])
 
     return (
@@ -23,6 +29,19 @@ const PostIdPage = () => {
             {isLoading
                 ? <Loader/>
                 : <div>{post.id}. {post.title}</div>
+            }
+            <h1>
+                Comments:
+            </h1>
+            {isComLoading
+                ? <Loader/>
+                : comments.map(comm => {
+                    return (<div style={{marginTop: "15px"}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )
+                })
             }
         </div>
     );
